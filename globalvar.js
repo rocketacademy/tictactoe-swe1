917171bc48c2827189d9a54ee236b4f46f85116c
jokatty <jyotikattani@gmail.com>
2021-04-18 10:30:32 +0800

    initial commit: working example code

---
+++ globalvar.js
@@ -0,0 +1,18 @@
+// Please implement exercise logic here
+// keep data about the game in a 2-D array
+const board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
+];
+
+// the element that contains the rows and squares
+let boardElement;
+
+// the element that contains the entire board
+// we can empty it out for convenience
+let boardContainer;
+
+// current player global starts at X
+let currentPlayer;
+currentPlayer = 'X';
--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-// Please implement exercise logic here
+// completely rebuilds the entire board every time there's a click
+const buildBoard = (board) => {
+  // start with an empty container
+  boardContainer.innerHTML = '';
+  boardElement = document.createElement('div');
+  boardElement.classList.add('board');
+
+  // move through the board data array and create the
+  // current state of the board
+  for (let i = 0; i < board.length; i += 1) {
+    // separate var for one row / row element
+    const row = board[i];
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('row');
+
+    // set each square
+    // j is the column number
+    for (let j = 0; j < row.length; j += 1) {
+      // one square element
+      const square = document.createElement('div');
+      square.classList.add('square');
+
+      // set the text of the square according to the array
+      square.innerText = board[i][j];
+
+      rowElement.appendChild(square);
+
+      // set the click all over again
+      // eslint-disable-next-line
+      square.addEventListener('click', () => {
+        squareClick(i, j);
+      });
+    }
+
+    // add a single row to the board
+    boardContainer.appendChild(rowElement);
+  }
+};
+
+// create the board container element and put it on the screen
+const initGame = () => {
+  boardContainer = document.createElement('div');
+  document.body.appendChild(boardContainer);
+
+  // build the board - right now it's empty
+  buildBoard(board);
+};
+
+// switch the global values from one player to the next
+const togglePlayer = () => {
+  if (currentPlayer === 'X') {
+    currentPlayer = 'O';
+  } else {
+    currentPlayer = 'X';
+  }
+};
+
+const squareClick = (column, row) => {
+  console.log('coordinates', column, row);
+
+  // see if the clicked square has been clicked on before
+  if (board[column][row] === '') {
+    // alter the data array, set it to the current player
+    board[column][row] = currentPlayer;
+
+    // refresh the creen with a new board
+    // according to the array that was just changed
+    buildBoard(board);
+
+    // change the player
+    togglePlayer();
+  }
+};
+
+initGame();
